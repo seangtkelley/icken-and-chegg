6e77dea0b403ce780c060c1b0ef20bd0daf83113
Sean Kelley <seangtkelley@gmail.com>
2018-09-26 22:09:34 -0400

    python3 in train script

--- scripts/train_yolo_keras.sh
+++ scripts/train_yolo_keras.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python /home/sgkelley/keras-yolo3/train.py --train_annots "/home/sgkelley/config/yolo_608window_train.txt" --val_annots "/home/sgkelley/config/yolo_608window_val.txt" --classes "/home/sgkelley/config/word_class.txt" 
+python3 /home/sgkelley/keras-yolo3/train.py --train_annots "/home/sgkelley/config/yolo_608window_train.txt" --val_annots "/home/sgkelley/config/yolo_608window_val.txt" --classes "/home/sgkelley/config/word_class.txt" 
 
 exit
