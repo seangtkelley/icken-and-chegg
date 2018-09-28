d9ffebfd06f50393d01d46842b23bfa65be62180
Sean Kelley <seangtkelley@gmail.com>
2018-09-28 14:06:42 -0400

    fixed output_dir arg name

--- scripts/train_yolo_keras.sh
+++ scripts/train_yolo_keras.sh
@@ -3,7 +3,7 @@
 python3 /home/sgkelley/keras-yolo3/train.py --train_annots "/home/sgkelley/config/yolo_608window_train.txt" \
                                             --val_annots "/home/sgkelley/config/yolo_608window_val.txt" \
                                             --classes "/home/sgkelley/config/word_class.txt" \
-                                            --output_path "/home/sgkelley/output/keras_yolo3/baseline_09282018/" \
+                                            --output_dir "/home/sgkelley/output/keras_yolo3/baseline_09282018/" \
                                             --input_size 608
 
 exit
