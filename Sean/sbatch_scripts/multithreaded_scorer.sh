0447d6c7c2f09c388b817b34be8b43b9d90f2019
Sean Kelley <seangtkelley@gmail.com>
2018-12-16 11:25:54 -0500

    changed python version for scorer script

--- Sean/sbatch_scripts/multithreaded_scorer.sh
+++ Sean/sbatch_scripts/multithreaded_scorer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 /home/sgkelley/sean/cascaded-faster-rcnn/evaluation/multithreaded_scorer.py
+python /home/sgkelley/sean/cascaded-faster-rcnn/evaluation/multithreaded_scorer.py
 
 exit
