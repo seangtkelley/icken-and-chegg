dd9c249ce2d7cd031efbd564dd8bb8c8af695fd2
Sean Kelley <seangtkelley@gmail.com>
2018-12-18 00:17:43 -0500

    generate preds now has options and outputs .npy files

--- Sean/python_scripts/generate_tbpp_preds.py
+++ Sean/python_scripts/generate_tbpp_preds.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import cv2
 import glob
+from optparse import OptionParser
 
 home_dir = os.path.expanduser("~")
 ssd_detectors_dir = os.path.join(home_dir, 'sean', 'ssd_detectors')
@@ -13,39 +14,39 @@ from tbpp_model import TBPP512, TBPP512_dense
 from utils.model_utils import load_weights, calc_memory_usage
 from ssd_data import preprocess
 from tbpp_utils import PriorUtil
+from utils.bboxes import rbox3_to_polygon
 
 sys.path.append(os.path.join(home_dir, 'sean', 'cascaded-faster-rcnn', 'evaluation'))
 from util import rotate_image, adjust_image_size
 
-output_dir = os.path.join(home_dir, 'sean', 'output')
+parser = OptionParser()
+parser.add_option("-o", "--output_dir", help="output file", default="~/sean/output/tbpp/np_preds/")
+parser.add_option("-w", "--weights_file", help="file with model weights", default="~/sean/ssd_detectors/checkpoints/201807091503_dsodtbpp512fl_synthtext/weights.018.h5")
+parser.add_option("-i", "--images_dir", help="map images directory", default="~/data/maps/")
+parser.add_option("-p", "--preprocess", help="whether or not to preform same preprocess as done in original implementations (background removal, etc...)", type=bool, default=False)
+parser.add_option("-t", "--test_split", help="file from torch_phoc with test split", default=None)
+parser.add_option("-m", "--confidence", help="confidence threshold for predictions", type=float, default=0.8)
+parser.add_option("-r", "--rotate", help="whether or not to rotate image", type=bool, default=False)
 
-checkpoint_dir = os.path.join(home_dir, 'sean', 'ssd_detectors', 'checkpoints', '201807091503_dsodtbpp512fl_synthtext')
-#checkpoint_dir = os.path.join(home_dir, 'sean', 'output', 'tbpp', 'checkpoints', '201812142148_tbpp512fl_maps')
+(options, args) = parser.parse_args()
+
+weights_path = options.weights_file
+output_dir = options.output_dir
+map_images_dir = options.images_dir
+do_preprocess = options.preprocess
+test_split_file = options.test_split
+confidence_threshold = options.confidence
+rotate_image = options.rotate
 
 # TextBoxes++ + DenseNet
 model = TBPP512_dense(softmax=False)
-
-#weights_path = os.path.join(checkpoint_dir, 'weights.005.h5')
-weights_path = os.path.join(checkpoint_dir, 'weights.018.h5')
-#confidence_threshold = 0.5
-confidence_threshold = 0.8
 
 load_weights(model, weights_path)
 
 prior_util = PriorUtil(model)
 
-map_images_dir = os.path.join(home_dir, 'data', 'maps')
-do_preprocess = False
-
-#preds_output_path = os.path.join(output_dir, 'tbpp', 'map_trained_angles_tbpp_0.5_preds_all.txt')
-preds_output_path = os.path.join(output_dir, 'tbpp', 'synthtext_trained_tbpp_0.8_preds_all.txt')
-preds_output_file = open(preds_output_path, "w+")
-
-test_only = False
 test_filenames = []
-if test_only:
-    test_split_file = os.path.join(home_dir, 'torch-phoc', 'splits', 'test_files.txt')
-
+if test_split_file:
     with open(test_split_file) as f:
         test_filenames = [line.replace("\n", "") for line in f.readlines()]
 
@@ -53,28 +54,26 @@ crop_h = 512
 crop_w = 512
 step = 400
 
-for filepath in glob.glob(os.path.join(map_images_dir, 'D*')):
-    if test_only:
-        filename = filepath.split('/')[-1]
-        head, _, _ = filename.partition('.')
-        name = head.split("_")[0]
+angles = [0] if rotate_image else range(-90, 95, 5)
 
-        if name not in test_filenames:
+for filepath in glob.glob(os.path.join(map_images_dir, 'D*')):
+    filename = filepath.split('/')[-1]
+
+    if test_split_file:
+        if filename.split('.')[0] not in test_filenames:
             continue
 
     print(filepath)
     map_img = cv2.imread(filepath)
     original_shape = map_img.shape
 
-    filename = filepath.split('/')[-1]
-    preds_output_file.write(filename + "\n")
-    
-    for angle in range(-90, 95, 5):
+    preds = []
+    confs = []
+    for angle in angles:
         rot_img, rot_mat, bounds = rotate_image(map_img, angle, original_shape)
         height = rot_img.shape[0]
         width = rot_img.shape[1]
         current_x = 0; current_y = 0
-        preds = []
 
         while current_y + crop_h < height:
             while current_x + crop_w < width:
@@ -84,38 +83,57 @@ for filepath in glob.glob(os.path.join(map_images_dir, 'D*')):
                 if do_preprocess:
                     crop_img = preprocess(crop_img, (512, 512))
                 
-                model_output = model.predict(np.array([crop_img]), batch_size=1, verbose=1)
+                model_output = model.predict(np.array([crop_img]), batch_size=1, verbose=0)
                 
                 res = prior_util.decode(model_output[0], confidence_threshold, fast_nms=False)
                 bboxes = res[:,0:4]
                 quades = res[:,4:12]
                 rboxes = res[:,12:17]
-                    
-                for j in range(len(bboxes)): # xmin, ymin, xmax, ymax
-                    # scale bbox
-                    bbox = bboxes[j]*512
-                    
-                    # translate points
-                    crop_x_min = bbox[0] + current_x
-                    crop_y_min = bbox[1] + current_y
-                    crop_x_max = bbox[2] + current_x
-                    crop_y_max = bbox[3] + current_y
-                    
-                    # find width and height
-                    w = crop_x_max - crop_x_min
-                    h = crop_y_max - crop_y_min
+                conf = res[:,17]
 
-                    preds.append( (crop_x_min, crop_y_max, w, h) )
-                
+                for j in range(len(rboxes)):
+                    # convert rbox
+                    polygon = rbox3_to_polygon(rboxes[j])*512
+
+                    # sort points
+                    xmin = np.min(polygon[:, 0])
+                    xmax = np.max(polygon[:, 0])
+                    ymin = np.min(polygon[:, 1])
+                    ymax = np.max(polygon[:, 1])
+
+                    # translate to full image location
+                    xmin += current_x
+                    xmax += current_x
+                    ymin += current_y
+                    ymax += current_y
+
+                    # recreate polygon
+                    polygon = np.reshape([xmin, ymax, xmax, ymax, xmax, ymin, xmin, ymin], (-1,2))
+
+                    # rotate to orientation when image is not rotated
+                    image_center = (original_shape[1] // 2, original_shape[0] // 2)
+                    rot_mat = cv2.getRotationMatrix2D(image_center, -1*angle, scale=1.0)
+
+                    # add col for rotation
+                    polygon = np.concatenate([polygon, np.ones([polygon.shape[0], 1])], axis=1)
+
+                    # rotate
+                    transformed_points = rot_mat.dot(polygon.T).T
+
+                    pt1 = [int(transformed_points[0][0]), int(transformed_points[0][1])]
+                    pt2 = [int(transformed_points[1][0]), int(transformed_points[1][1])]
+                    pt3 = [int(transformed_points[2][0]), int(transformed_points[2][1])]
+                    pt4 = [int(transformed_points[3][0]), int(transformed_points[3][1])]
+
+                    preds.append( [pt1, pt2, pt3, pt4] )
+                    conf.append( conf[j] )
+
                 current_x += step
 
             current_x = 0
             current_y += step
         
-        print("Found", str(len(preds)), "boxes for angle", str(angle))
-        preds_output_file.write("angle " + str(angle) + "\n")
-        preds_output_file.write(str(len(preds)) + "\n")
-        preds_output_file.write("\n".join([" ".join(map(str, bbox)) for bbox in preds]) + "\n")
+        print("Found", str(len(rbox3_to_polygon)), "boxes for angle", str(angle))
 
-    
-preds_output_file.close()
+    np.save(os.path.join(output_dir, filename.split('.')[0]+'.npy'), preds)
+    np.save(os.path.join(output_dir, filename.split('.')[0]+'_scores.npy'), confs)
